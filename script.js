7c3eae76e26d6d5c8795a767c2c09bd98e718f4b
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-12-15 16:47:35 -0300

    criar todos os elemetos html via javascript

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 
+let body = document.querySelector('body');
+
+let inputText = document.createElement('input');
+inputText.type = 'text';
+inputText.id = 'text-input';
+inputText.maxLength = '60';
+inputText.placeholder = 'Digite seu texto aqui';
+body.appendChild(inputText);
+
+let inputImage = document.createElement('input');
+inputImage.id = 'meme-insert';
+inputImage.type = 'file';
+body.appendChild(inputImage);
+
+let container = document.createElement('div');
+container.id = 'meme-image-container';
+body.appendChild(container);
+
+let textDiv = document.createElement('div');
+textDiv.id = 'meme-text';
+container.appendChild(textDiv);
+
+let imageDiv =document.createElement('div');
+imageDiv.id = 'meme-image';
+container.appendChild(imageDiv);
+
 let textInput = document.getElementById('text-input');
 textInput.addEventListener('keyup', printText);
 function printText(){
@@ -11,8 +37,8 @@ let memeInsert = document.getElementById('meme-insert');
 
 memeInsert.addEventListener('change', loadFile);
 
-function loadFile(event){                           // ref.: https://www.codegrepper.com/code-examples/javascript/display+image+upload+javascript
+function loadFile(){                           // ref.: https://www.codegrepper.com/code-examples/javascript/display+image+upload+javascript
     let img = document.createElement('img');
     memeImage.appendChild(img);
-    img.src = URL.createObjectURL(event.target.files[0]);
-}
+    img.src = URL.createObjectURL(memeInsert.files[0]); 
+} 

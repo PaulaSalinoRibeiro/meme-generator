ac6090e50ac6495f25e29b77af973ff13a11c55c
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-12-15 20:19:46 -0300

    requisito 6 ok

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ body.appendChild(earthButton);
 
 fireButton.addEventListener('click', styleFire);
 function styleFire(){
-    container.style.border = '3px dashed red'
+    container.style.border = '3px dashed red';
 }
 
 waterButton.addEventListener('click', styleWater);
@@ -74,4 +74,59 @@ function styleWater(){
 earthButton.addEventListener('click', styleEarth);
 function styleEarth(){
     container.style.border = '6px groove green'
+}
+
+let divMemes = document.createElement('div');
+body.appendChild(divMemes);
+
+let meme1 = document.createElement('img');
+meme1.id = 'meme-1';
+meme1.src = '/imgs/meme1.png';
+divMemes.appendChild(meme1);
+
+let meme2 = document.createElement('img');
+meme2.id = 'meme-2';
+meme2.src = '/imgs/meme2.png';
+divMemes.appendChild(meme2);
+
+let meme3 = document.createElement('img');
+meme3.id = 'meme-3';
+meme3.src = '/imgs/meme3.png';
+divMemes.appendChild(meme3);
+
+let meme4 = document.createElement('img');
+meme4.id = 'meme-4';
+meme4.src = '/imgs/meme4.png';
+divMemes.appendChild(meme4);
+
+meme1.addEventListener('click', addMeme1);
+
+function addMeme1(){
+    let img = document.createElement('img');
+    memeImage.appendChild(img);
+    img.src = '/imgs/meme1.png'
+}
+
+meme2.addEventListener('click', addMeme2);
+
+function addMeme2(){
+    let img = document.createElement('img');
+    memeImage.appendChild(img);
+    img.src = '/imgs/meme2.png'
+}
+
+meme3.addEventListener('click', addMeme3);
+
+function addMeme3(){
+    let img = document.createElement('img');
+    memeImage.appendChild(img);
+    img.src = '/imgs/meme3.png'
+}
+
+meme4.addEventListener('click', addMeme4);
+
+function addMeme4(){
+    let img = document.createElement('img');
+    memeImage.appendChild(img);
+    img.src = '/imgs/meme4.png'
 }
